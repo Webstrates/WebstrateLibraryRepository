ecafcd463f0c809f3f6aebcf5b76a7ee5c3da181
CAVI-Jenkins <support@cavi.au.dk>
2023-09-29 12:30:19 +0000

    Automatic commit

--- AssetFetcher/assetfetcher.js
+++ AssetFetcher/assetfetcher.js
@@ -1,19 +1,22 @@
 window.WPMv2.installAsset = async function installAsset(destination, source){
     if(typeof webstrate  !== "undefined") {
-        if (!webstrate.assets.find(a=>a.fileName==destination)){
+        let asset = webstrate.assets.findLast(a=>a.fileName==destination);
+        if ((!asset) || (asset.deletedAt)){
             console.log("Initial fetch of asset "+destination+" from "+source+" ...");
             let formData = new FormData();
             
             let response = await fetch(source, {credentials: 'same-origin'});
-            let blob = await response.blob();            
+            let blob = await response.blob();          
             formData.append("file", blob, destination);
             await fetch(location.pathname, {
                 body: formData,
                 credentials: 'same-origin',
                 method: "post"
             });            
+            return true;
         }
     } else {
         console.log("AssetFetcher: Don't know how to fetch asset "+destination+" when not using webstrates, please manually download it from "+source);
     }
+    return false;
 }
